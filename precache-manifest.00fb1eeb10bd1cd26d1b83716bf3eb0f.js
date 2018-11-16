56461cbf6bd7eaf97e059dca6aad187bdae1d3d1
Filipe Carvalho <fullipecarvalho@bitbucket.org>
2018-11-16 20:28:49 -0200

    Updates

---
+++ precache-manifest.00fb1eeb10bd1cd26d1b83716bf3eb0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6f2fe2aa6271300b2038",
+    "url": "/contactmanager/static/js/main.6f2fe2aa.chunk.js"
+  },
+  {
+    "revision": "d4c686c74563c6df5ddd",
+    "url": "/contactmanager/static/js/1.d4c686c7.chunk.js"
+  },
+  {
+    "revision": "6f2fe2aa6271300b2038",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "d4c686c74563c6df5ddd",
+    "url": "/contactmanager/static/css/1.b1087bab.chunk.css"
+  },
+  {
+    "revision": "f4214462b31777a5bc724d4d6d28cdb3",
+    "url": "/contactmanager/index.html"
+  }
+];
